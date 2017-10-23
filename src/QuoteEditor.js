48f4dad3816019cf2572e5e04bb683d106ec8961
ahw <andrewhallagan@gmail.com>
2017-10-22 22:55:11 -0400

    Move quote editor styles

--- src/QuoteEditor.js
+++ src/QuoteEditor.js
@@ -1,16 +1,6 @@
 import React from 'react';
 
 export default function QuoteEditor(props) {
-    const style = {
-        width: '100%',
-        height: '30vh',
-        border: 'none',
-        borderBottom: '1px solid gray',
-        outline: 'none',
-        boxSizing: 'border-box',
-        padding: 5,
-        fontSize: 16,
-    }
-    return <textarea style={style} value={props.rawText} onChange={props.onChange}/>
+    return <textarea id="main-quote-editor" value={props.rawText} onChange={props.onChange}/>
 }
 
