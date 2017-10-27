2e588e80c0f619a015fb988c80e9031186a47696
ahw <andrewhallagan@gmail.com>
2017-10-26 20:17:20 -0400

    Use defaultValue instead

--- src/LayoutEditor.js
+++ src/LayoutEditor.js
@@ -21,14 +21,14 @@ export default function LayoutEditor(props) {
     }
     
     return (<div style={{padding: 2}}>
-        <Control label="Extra CSS" type="text" value={props.extraCss} onChange={props.onCssChange.bind(this)} />
-        <Control label="Padding" type="range" min={0} max={200} value={props.layoutStyles.padding} onChange={props.onLayoutChange.bind(this, 'padding')} />
-        <Control label="Background color" type="text" value={props.layoutStyles.backgroundColor} onChange={props.onLayoutChange.bind(this, 'backgroundColor')} />
-        <Control label="Text color" type="text" value={props.layoutStyles.color} onChange={props.onLayoutChange.bind(this, 'color')} />
-        <Control label="Margin" type="range" value={props.layoutStyles.margin} min={0} max={50} onChange={props.onLayoutChange.bind(this, 'margin')} />
-        <Control label="Bottom margin" type="range" value={props.layoutStyles.marginBottom} min={-40} max={50} onChange={props.onLayoutChange.bind(this, 'marginBottom')} />
+        <Control label="Extra CSS" type="text" defaultValue={props.extraCss} onChange={props.onCssChange.bind(this)} />
+        <Control label="Padding" type="range" min={0} max={200} defaultValue={props.layoutStyles.padding} onChange={props.onLayoutChange.bind(this, 'padding')} />
+        <Control label="Background color" type="text" defaultValue={props.layoutStyles.backgroundColor} onChange={props.onLayoutChange.bind(this, 'backgroundColor')} />
+        <Control label="Text color" type="text" defaultValue={props.layoutStyles.color} onChange={props.onLayoutChange.bind(this, 'color')} />
+        <Control label="Margin" type="range" defaultValue={props.layoutStyles.margin} min={0} max={100} onChange={props.onLayoutChange.bind(this, 'margin')} />
+        <Control label="Bottom margin" type="range" defaultValue={props.layoutStyles.marginBottom} min={-40} max={100} onChange={props.onLayoutChange.bind(this, 'marginBottom')} />
         <Control label="Toggle overlay" type="checkbox" value={props.showOverlay} onChange={props.onOverlayToggle} />
-        <Control label="Invert colors" type="checkbox" value={props.invertColors} onChange={props.onInvertColors} />
+        <Control label="Invert colors" type="checkbox" defaultValue={props.invertColors} onChange={props.onInvertColors} />
     </div>);
 }
 
