abe9b2ff3ec60f8a22afc559b42cf888204cde31
ahw <andrewhallagan@gmail.com>
2017-10-28 10:41:12 -0400

    Re-order controls

--- src/LayoutEditor.js
+++ src/LayoutEditor.js
@@ -22,15 +22,15 @@ export default function LayoutEditor(props) {
 
     return (<div style={{padding: 5}}>
         <Control label="Extra CSS" type="text" value={props.extraCss} onChange={props.onCssChange.bind(this)} />
-        <Control label="Padding" type="range" min={0} max={200} value={props.layoutStyles.padding} onChange={props.onLayoutChange.bind(this, 'padding')} />
-        <Control label="Background color" type="color" value={props.layoutStyles.backgroundColor} onChange={props.onLayoutChange.bind(this, 'backgroundColor')} />
-        <Control label="Text color" type="color" value={props.layoutStyles.color} onChange={props.onLayoutChange.bind(this, 'color')} />
-        <Control label="Quote spacing" type="range" value={props.layoutStyles.marginBottom} min={-40} max={100} onChange={props.onLayoutChange.bind(this, 'marginBottom')} />
-        <Control label="Toggle overlay" type="checkbox" value={props.showOverlay} onChange={props.onOverlayToggle} />
         <Control label="Invert colors" type="checkbox" value={props.invertColors} onChange={props.onInvertColors} />
+        <Control label="Toggle overlay" type="checkbox" value={props.showOverlay} onChange={props.onOverlayToggle} />
         <Control label="Background image" type="file" onChange={props.onFileSelect} />
         <Control label="background-repeat" type="text" value={props.layoutStyles.backgroundRepeat} onChange={props.onLayoutChange.bind(this, 'backgroundRepeat')} />
         <Control label="background-position" type="text" value={props.layoutStyles.backgroundPosition} onChange={props.onLayoutChange.bind(this, 'backgroundPosition')} />
         <Control label="background-size" type="text" value={props.layoutStyles.backgroundSize} onChange={props.onLayoutChange.bind(this, 'backgroundSize')} />
+        <Control label="Background color" type="color" value={props.layoutStyles.backgroundColor} onChange={props.onLayoutChange.bind(this, 'backgroundColor')} />
+        <Control label="Text color" type="color" value={props.layoutStyles.color} onChange={props.onLayoutChange.bind(this, 'color')} />
+        <Control label="Quote spacing" type="range" value={props.layoutStyles.marginBottom} min={-40} max={100} onChange={props.onLayoutChange.bind(this, 'marginBottom')} />
+        <Control label="Side margins" type="range" min={0} max={200} value={props.layoutStyles.padding} onChange={props.onLayoutChange.bind(this, 'padding')} />
     </div>);
 }
